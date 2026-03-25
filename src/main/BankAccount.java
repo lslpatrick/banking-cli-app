f90f98684e71bc7c30477e2cff1f263ccf6e6e21
WilliamHong <143476386+williamHong6@users.noreply.github.com>
2026-03-25 15:24:58 -0500

    viewTransaction Completed

--- src/main/BankAccount.java
+++ src/main/BankAccount.java
@@ -1,16 +1,23 @@
 package main;
 
+import java.util.List;
+
 public class BankAccount {
 
     private double balance;
+    private List<String> transactionHistory;
+
 
     public BankAccount() {
         this.balance = 0;
+        this.transactionHistory = new java.util.ArrayList<>();
     }
 
     public void deposit(double amount) {
         if(amount > 0) {
             this.balance += amount;
+            this.transactionHistory.add("Deposited: $" + amount); //add desposit to transaction history 
+
         } else {
             throw new IllegalArgumentException();
         }
@@ -19,6 +26,7 @@ public class BankAccount {
     public void withdraw(double amount) {
         if(amount > 0 && amount <= this.balance) {
             this.balance -= amount;
+            this.transactionHistory.add("Withdrew: $" + amount); //add withdraw to transaction history
         } else {
             throw new IllegalArgumentException();
         }
@@ -26,5 +34,9 @@ public class BankAccount {
 
     public double getBalance() {
         return this.balance;
+    }
+
+    public List<String> getTransactionHistory() {
+        return this.transactionHistory;
     }
 }
